8c026d32a4b66cb3c460b9e314beb85ed0154e5c
Xie Xiaolei <xie.xiaolei@gmail.com>
2013-12-17 01:41:21 +0100

    The calculation of K is yet to be made tractable.

--- RealWishartCDF.m
+++ RealWishartCDF.m
@@ -22,14 +22,49 @@ K = K/Gamma(nmin, nmin/2);
 K1 = K*2^(alpha*nmat + nmat*(nmat+1)/2) * prod(alpha + [1:nmat]);
 A = zeros(nmat, nmat, length(x));
 b = NaN(length(x), nmin);
-for i = 1:nmin
-    b(:, i) = P(alpha + i, x/2).^2 / 2;
-    for j = i:nmin-1
-        b(:, j+1) = b(:, j) - 2^(-2*alpha-i-j) *...
-            gamma(2*alpha+i+j) / gamma(alpha+i) / gamma(alpha+j)...
-            * P(2*alpha+i+j, x);
-        A(i, j+1, :) = P(alpha+i, x/2) .* P(alpha+j+1, x/2)...
-            - 2*b(:, j+1);
+for u = 1:nmin
+    b(:, u) = (P(alpha+u, x/2)).^2 / 2;
+    m = 2*(alpha-1/2) + 2*u;
+    for v = u : nmin-1
+        l = v - u;
+        if mod(2*alpha, 2) == 1 && mod(l, 2) == 1
+            k = 1:m/2;
+            s1 = prod(2*k ./ (2*k-1));
+
+            k = 1:(l+1)/2;
+            s2 = prod((m+2*k-2) ./ (m+l+2*k));
+            
+            s = s1 * s2 /(m*pi);
+        elseif mod(2*alpha, 2) == 1 && mod(l, 2) == 0
+            k = 1:m/2;
+            s1 = prod(2*k ./ (2*k-1));
+            
+            if l == 0
+                s2 = 1;
+            else
+                k = 1 : l/2;
+                s2 = prod((m+2*k) ./ (m+l+2*k+1));
+            end
+            s = s1 * s2 / (m+l+1) / pi;
+        elseif mod(2*alpha, 2) == 0 && mod(l, 2) == 1
+            k = 1:m/2-1;
+            s1 = prod(2*k ./ (2*k-1));
+            
+            k = 1:(l+1)/2;
+            s2 = prod((m+2*k-3) ./ (m+l+2*k-1));
+            s = s1 * s2 / 2;
+        elseif mod(2*alpha, 2) == 0 && mod(l, 2) == 0
+            k = 1:m/2-1;
+            s1 = prod(2*k ./ (2*k-1));
+            
+            k = 1:l/2+1;
+            s2 = prod((m+2*k-3) ./ (m+l+2*k-2));
+            s = s1 * s2 / 2;
+        end
+        b(:, v+1) = b(:, v) - s .* P(2*alpha + (u+v), x);
+        A(u, v+1, :) = ...
+            P(alpha+u, x/2) .* P(alpha+v+1, x/2)...
+            - 2*b(:, v+1);
     end
 end
 if mod(nmin, 2) == 1
--- RealWishart_P.m
+++ RealWishart_P.m
@@ -1,63 +1,41 @@
 function y = RealWishart_P(ain, x)
 persistent a1 a n ya k u1 u2;
 
-if isempty(a1) || ain ~= a1
-    a1 = ain;
-    ya = NaN(size(x));
-    if mod(a1*2, 2) == 0
-        n = a1;
-        a = 0;
-        ya = ones(size(x));
-    else
-        n = a1 - 1/2;
-        a = 1/2;
-        ya = 2*normcdf(sqrt(2*x)) - 1;
-    end
-    % u1 = factorial(k);
-    % u2 = NaN(size(k));
-    % for m = 1:length(k)
-    %     u2(m) = sqrt(pi) * prod(1:2:(2*k(m)+1)) / 2^(k(m)+1);
-    % end
-end
-
-y1 = NaN(size(x));
-
+% if isempty(a1) || ain ~= a1
+%     a1 = ain;
+%     ya = NaN(size(x));
+%     if mod(a1*2, 2) == 0
+%         n = a1;
+%         a = 0;
+%         ya = ones(size(x));
+%     else
+%         n = a1 - 1/2;
+%         a = 1/2;
+        
+%     end
+%     % k = [0:n-1];
+%     % u1 = factorial(k);
+%     % u2 = NaN(size(k));
+%     % for m = 1:length(k)
+%     %     u2(m) = sqrt(pi) * prod(1:2:(2*k(m)+1)) / 2^(k(m)+1);
+%     % end
+% end
+y = NaN(size(x));
 if a == 0
     for j = 1 : length(x)
-        y1(j) = 0;
-        l = 0;
-        done = 0;
-        while ~done
-            inc = 0;
-            for k = 0 : min(n-1, l)
-                inc = inc + prod(x(j) ./ [1:k, 1:l-k]) * (-1)^(l-k);
-            end
-            y1(j) = y1(j) + inc;
-            if abs(inc) > 0.05
-                l = l + 1;
-            else
-                done = 1;
-            end
+        inc = 0;
+        for k = 0:n-1
+            inc = inc + prod(x(j)./[1:k]);
         end
+        y(j) = 1 - inc / exp(x(j));
     end
 else
     for j = 1 : length(x)
-        y1(j) = 0;
-        l = 0;
-        done = 0;
-        while ~done
-            inc = 0;
-            for k = 0:l
-                inc = inc + prod(x(j) ./ [1:l, [1:2*k+1]./2]) * (-1)^l;
-            end
-            y1(j) = y1(j) + inc;
-            if abs(inc) > 0.05
-                l = l + 1;
-            else
-                done = 1;
-            end
+        inc = 0;
+        for k = 0:n-1
+            inc = inc + prod(x(j)./[1/2:1:k+1/2]);
         end
-        y1(j) = y1(j) /sqrt(x(j)) / sqrt(pi);
+        y(j) = erf(sqrt(x(j))) - inc/exp(x(j))/sqrt(pi*x(j));
     end
 end
-y = ya - y1;
+
