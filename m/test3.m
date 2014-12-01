bbf50de14354a59fb3d7584afd0dcb5e808e4024
Xie Xiaolei <xie.xiaolei@gmail.com>
2014-12-01 10:24:12 +0100

    Modified matlab scripts for Lund project.

--- m/test3.m
+++ m/test3.m
@@ -16,7 +16,7 @@ spec = cellstr(['b  '; 'c  '; 'g  '; 'm  '; 'r  '; 'k  ';...
 texts = {};
 c = 1;
 % hold on
-q = 0.2;
+q = 0.5;
 lam1 = NaN(length(sig), length(phi), 2);
 lam2 = NaN(length(sig), length(phi), 2);
 variance = NaN(length(sig), length(phi));
@@ -55,33 +55,16 @@ B(:, 1) = B(I, 1);
 B(:, 2) = reshape(lam2(:, :, 2), numel(lam2(:, :, 2)), 1);
 B(:, 2) = B(I, 2);
 
-% a = (1 - sqrt(q))^2;
-% b = (1 + sqrt(q))^2;
-% Z1 = linspace(a, b, 1000);
-% MPGreen = @(z) (z + q - 1 - sqrt((z-a).*(z-b)))./(2*q.*z);
-% g = MPGreen(Z1);
-% plot(Z1, -imag(g)./pi, 'r', 'LineWidth', 2);
+f = @(param, X) param(1) .* exp(param(2) .* X) + param(3);
+param1 = lsqcurvefit(f, [1, 1, (1 - sqrt(q))^2], variance, A(:, 1));
+param2 = lsqcurvefit(f, [1, 1, (1 + sqrt(q))^2], variance, B(:, 1));
 
-% F1 = cumsum((-imag(g)./pi))*(Z1(2) - Z1(1));
-% F1 = F1 ./ F1(end);
-% plot(log10(Z1), log10(1-F1), 'r', 'LineWidth', 2);
-% hold off
-
-% xlabel('Eigenvalues');
-% ylabel('Probability Densities of Eigenvalues');
-% xlim([0, 10]);
-% ylim([0, 1.4]);
-% grid on
-    
-% [Z2, P2] = epdf(ev, 1, min(ev), max(ev), 500, '');
-% plot((Z2), (P2), 'ro');
-% hold on
-
-% plot((Z1), (-imag(G1)./pi), 'b', 'LineWidth', 2);
-% hold off
-
-% grid on
-% legend(texts);
-% ylabel('Tail function of eigenvalue distribution');
+X = linspace(min(variance), max(variance), 500)';
+semilogx(variance, B(:, 1), '+', X, f(param2, X), variance, B(:, 2), '-o');
+grid on
+legend('estimated E(\lambda_{max})', 'Fit to exponential', ['Linear ' ...
+                    'approximation'], 'Location', 'Northwest');
+xlabel('log(v)');
+ylabel('E (\lambda_{max})');
 
 
