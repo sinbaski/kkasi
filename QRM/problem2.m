f51a49ece85a03d0917b22932ec08d73ec39fc5e
lxb353 <lxb353@ML02240.science.domain>
2014-12-05 21:29:43 +0100

    QRM problem 2.

--- QRM/problem2.m
+++ QRM/problem2.m
@@ -1,12 +1,23 @@
 clear all
 close all
-L = importdata('DAX.txt', '\t');
-L = -L(:, 2);
+L1 = importdata('DAX.txt', '\t');
+L1 = -L1(:, 2);
+
+%% compound into 10-day returns
+L10 = NaN(length(L1)/10, 1);
+for j = 1:length(L1)/10
+    L10(j) = sum(L1((j-1)*10+1 : j*10));
+end
+
+L = L10;
+
 n = length(L);
 alfa = 0.99;
 k = ceil(n*(1 - alfa));
 Ls = sort(L, 'descend');
 Ls(k);
+
+fprintf('The VaR is the %dth upper order statistic.\n', k);
 
 accum = 0;
 bita = 5.0e-2;
@@ -30,4 +41,8 @@ while accum <= bita/2
     B = n-m+1:n;
     accum = accum + prod(B ./ A .* (1 - alfa)) * alfa^(n-m);
 end
-m = m - 1;
+if m > 0
+    m = m - 1;
+end
+
+fprintf('The upper bound is the %dth upper order statistic.\n', m+1);
