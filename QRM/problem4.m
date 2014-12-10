54d3bb96633919b4cdf0a8d734c147e273c68726
Xie Xiaolei <xie.xiaolei@gmail.com>
2014-12-10 01:23:01 +0100

    Added the solution to problem 4.

--- QRM/problem4.m
+++ QRM/problem4.m
@@ -37,4 +37,20 @@ end
 plot(X, Y, '+');
 xlabel('u');
 ylabel('Mean Excess function e(u)');
-I = X > 0.02;
+I = X > 0.01;
+m3 = skewness(X(I)-0.01);
+xi = fsolve(@(x) 2*(1 + x)*sqrt(1 - 2*x) - (1 - 3*x)*m3, 1/4);
+sig = sqrt(var(X(I) - 0.01)/(1 - xi^2)/(1 - 2*xi));
+mu = mean(X(I) - 0.01) - sig/(1 - xi);
+
+[param, pci] = mle(X(I)-0.01, 'distribution', 'gp');
+[nlogL, acov] = gplike(param, X(I));
+
+
+[y, x] = ecdf(X(I) - 0.01);
+yi = gpcdf(x, param(1), param(2), 0);
+plot(x, 1 - y, x, 1 - yi);
+grid on
+xlabel('x');
+ylabel('1 - F(x+u)');
+legend('empirical', 'fitted GPD');
