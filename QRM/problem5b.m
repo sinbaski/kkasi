37dd9b0fc8c95575755c74de91880ee7977756a1
Xie Xiaolei <xie.xiaolei@gmail.com>
2014-12-15 11:34:45 +0100

    New additions.

--- QRM/problem5b.m
+++ QRM/problem5b.m
@@ -1,17 +1,23 @@
 clear all
 close all
 
+VaR = 219.27;
 S = [
     1, 1/4, 1/3
     1/4, 1, -1/5
     1/3, -1/5, 1]*1.0e-3;
+Mu = 1.0e-3 * [1/2, 1, 1]';
+% a = 1.0e+3;
+% b = (-VaR/a - sum(Mu))/()
 
-Mu = [0; 0; -219.27e-3];
-num = 1.0e+5;
-R = mvnrnd(Mu, S, num);
+
+Nu = [0; 0; -219.27e-3];
+num = 5.0e+3;
+R = mvnrnd(Nu, S, num);
 W = ones(1, 3);
-Ln1 = sort(-W * diag([1000, 1000, 1000]) * R');
-VaR = 219.27;
+Ln1 = sort(-sum(diag([1000, 1000, 1000]) * R'));
+
+hold on; plot(R(1, :), -VaR*1.0e-3 - R(1, :), 'r-');
 
 
 
--- m/test16.m
+++ m/test16.m
@@ -1,13 +1,45 @@
 clear all
 close all
-sig = 0.2;
-q = 0.1;
-v = sig^2;
+n = 500;
+X = linspace(-8, 1, n)';
+Y = linspace(-4, -1.0e-3, n)';
 
-a = LognormalEndsConstA(v, q);
-C1 = LognormalEndsConstC(v, q, a, 1);
-C3 = LognormalEndsConstC(v, q, a, 3);
-[u, lam2] = LognormalEnds(v, q);
+Bre = NaN(n, n);
+Bim = NaN(n, n);
+for k = 1 : n
+    M = LognormalBlue(X(k) + i*Y, 0.5, 0.5);
+    Bre(:, k) = M(:, 1);
+    Bim(:, k) = M(:, 2);
+end
+C = contour(X, Y, Bim, [0, 0]);
 
-blk = 3*sqrt(C3*q^2 + 1/a^3)/2 + (C1 + 1/a - lam2)^(3/2);
-lam1 = C1 + 1/a - blk^(2/3);
+subplot(1, 2, 1);
+contourf(X, Y, Bre, [-1:0.1:1]);
+colorbar('location', 'EastOutside');
+hold on
+plot(C(1, 2:end), C(2, 2:end), 'k-', 'Linewidth', 2);
+title('Re B');
+xlabel('Re G');
+ylabel('Im G');
+
+subplot(1, 2, 2);
+contourf(X, Y, Bim, [-1:0.1:1]);
+colorbar('location', 'EastOutside');
+title('Im B');
+xlabel('Re G');
+ylabel('Im G');
+
+
+
+
+% A = find(abs(Bim) < 1.0e-4);
+% C = NaN(length(A), 2);
+% for k = 1 : length(A)
+%     C(k, 1) = ceil(A(k)/n);
+%     C(k, 2) = mod(A(k), n);
+%     if (C(k, 2) == 0)
+%         C(k, 2) = n;
+%     end
+% end
+% hold on
+% plot(X(C(:, 1)), Y(C(:, 2)), 'k.', 'Linewidth', 2);
