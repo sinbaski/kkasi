6228bfff7794b3fdf6ade7d5e29b7fda3f79bdff
Xie Xiaolei <xie.xiaolei@gmail.com>
2014-12-22 01:12:41 +0100

    Work on the paper with Sven.

--- m/test3.m
+++ m/test3.m
@@ -16,58 +16,60 @@ spec = cellstr(['b  '; 'c  '; 'g  '; 'm  '; 'r  '; 'k  ';...
 texts = {};
 c = 1;
 % hold on
-q = 1;
 lam1 = NaN(length(sig), length(phi), 2);
 lam2 = NaN(length(sig), length(phi), 2);
 variance = NaN(length(sig), length(phi));
 
-for m = 1 : length(sig)
-    for n = 1 : length(phi)
-        % for sig = 0.1
-        v = sig(m)^2/(1 - phi(n)^2);
-        variance(m, n) = v;
-        load(sprintf(['../matfys/data/sv/normal_ret/lognormal_vol/q%.1f/Eig-' ...
-                      'sig%.4f-phi%.4f.mat'], q, sig(m), phi(n)), 'ev');
+for q = [0.1, 0.2, 0.5, 1]
+    for m = 1 : length(sig)
+        for n = 1 : length(phi)
+            % for sig = 0.1
+            v = sig(m)^2/(1 - phi(n)^2);
+            variance(m, n) = v;
+            load(sprintf(['../matfys/data/sv/normal_ret/lognormal_vol/q%.1f/Eig-' ...
+                          'sig%.4f-phi%.4f.mat'], q, sig(m), phi(n)), 'ev');
 
-        % ev = reshape(ev, 1, prod(size(ev)));
-        eigmax = max(ev)';
-        eigmin = min(ev)';
-        
-        lam2(m, n, :) = [mean(eigmax), (1+sqrt(q))^2 * ...
-                            (1 + 2*v*(2*sqrt(q)+1))];
-        lam1(m, n, :) = [mean(eigmin), (1-sqrt(q))^2 * ...
-                            (1 - 2*v*(2*sqrt(q)-1))];
-        
-        c = c + 1;
+            % ev = reshape(ev, 1, prod(size(ev)));
+            eigmax = max(ev)';
+            eigmin = min(ev)';
+            
+            lam2(m, n, :) = [max(eigmax), (1+sqrt(q))^2 * ...
+                             (1 + 2*v*(2*sqrt(q)+1))];
+            lam1(m, n, :) = [min(eigmin), (1-sqrt(q))^2 * ...
+                             (1 - 2*v*(2*sqrt(q)-1))];
+            
+        end
     end
+
+    [V, I] = sort(reshape(variance, numel(variance), 1));
+    A = NaN(numel(lam1(:, :, 1)), 2);
+    B = NaN(numel(lam2(:, :, 1)), 2);
+
+    A(:, 1) = reshape(lam1(:, :, 1), numel(lam1(:, :, 1)), 1);
+    A(:, 1) = A(I, 1);
+    A(:, 2) = reshape(lam1(:, :, 2), numel(lam1(:, :, 2)), 1);
+    A(:, 2) = A(I, 2);
+    B(:, 1) = reshape(lam2(:, :, 1), numel(lam2(:, :, 1)), 1);
+    B(:, 1) = B(I, 1);
+    B(:, 2) = reshape(lam2(:, :, 2), numel(lam2(:, :, 2)), 1);
+    B(:, 2) = B(I, 2);
+
+    % f = @(param, X) param(1) .* exp(param(2) .* X) + param(3);
+    % param1 = lsqcurvefit(f, [1, 1, (1 - sqrt(q))^2], V, A(:, 1));
+    % param2 = lsqcurvefit(f, [1, 1, (1 + sqrt(q))^2], V, B(:, 1));
+
+    % X = linspace(min(V), max(V), 500)';
+    % semilogx(V, A(:, 1), '+', X, f(param1, X), V, A(:, ...
+    %                                                   2), '-o');
+    subplot(2, 2, c);
+    semilogx(V, A(:, 1), 'g-', V, A(:, 2), 'r-');
+    grid on
+    % legend('simulated E(\lambda_{min})', 'Fit to exponential', ['Linear ' ...
+    %                    'approximation'], 'Location', 'Northwest');
+    xlabel('log(v)');
+    ylabel('min(\lambda_{max})');
+    title(sprintf('q = %.1f', q));
+    c = c + 1;
 end
-
-[variance, I] = sort(reshape(variance, numel(variance), 1));
-A = NaN(numel(lam1(:, :, 1)), 2);
-B = NaN(numel(lam2(:, :, 1)), 2);
-
-A(:, 1) = reshape(lam1(:, :, 1), numel(lam1(:, :, 1)), 1);
-A(:, 1) = A(I, 1);
-A(:, 2) = reshape(lam1(:, :, 2), numel(lam1(:, :, 2)), 1);
-A(:, 2) = A(I, 2);
-B(:, 1) = reshape(lam2(:, :, 1), numel(lam2(:, :, 1)), 1);
-B(:, 1) = B(I, 1);
-B(:, 2) = reshape(lam2(:, :, 2), numel(lam2(:, :, 2)), 1);
-B(:, 2) = B(I, 2);
-
-% f = @(param, X) param(1) .* exp(param(2) .* X) + param(3);
-% param1 = lsqcurvefit(f, [1, 1, (1 - sqrt(q))^2], variance, A(:, 1));
-% param2 = lsqcurvefit(f, [1, 1, (1 + sqrt(q))^2], variance, B(:, 1));
-
-% X = linspace(min(variance), max(variance), 500)';
-% semilogx(variance, A(:, 1), '+', X, f(param1, X), variance, A(:, ...
-%                                                   2), '-o');
-semilogx(variance, A(:, 1), 'g-', variance, A(:, 2), 'r-');
-grid on
-% legend('simulated E(\lambda_{min})', 'Fit to exponential', ['Linear ' ...
-%                    'approximation'], 'Location', 'Northwest');
-xlabel('log(v)');
-ylabel('E (\lambda_{min})');
-
 
 
--- m/test4.m
+++ m/test4.m
@@ -1,14 +1,54 @@
 clear all
 close all
 
-a = linspace(10^(-2.7), 10^(-2.65), 8000);
-for q = 0.1
-    for v = 0.5
-        % g = LognormalGreen(mean(eigmax), v, q);
-        I = LognormalEndsFun1(v, q, a);
-        plot(log10(a), log10(I), log10(a), -2*log10(a));
+phi = [0
+       0.25
+       0.5
+       0.7070
+       0.8160
+      ];
+sig = [0.1, 0.2, 0.5];
+spec = cellstr(['b  '; 'c  '; 'g  '; 'm  '; 'r  '; 'k  ';...
+                'b--'; 'c--'; 'g--'; 'm--'; 'r--'; 'k--';...
+                'b: '; 'c: '; 'g: '; 'm: '; 'r: '; 'k: ';...
+                'b-.'; 'c-.'; 'g-.'; 'm-.'; 'r-.'; 'k-.';...
+                ]);
+texts = {};
+c = 1;
+% hold on
+lam1 = NaN(length(sig), length(phi), 2);
+lam2 = NaN(length(sig), length(phi), 2);
+variance = NaN(length(sig), length(phi));
+
+q = 0.1;
+for m = 1 : length(sig)
+    for n = 1 : length(phi)
+        % for sig = 0.1
+        v = sig(m)^2/(1 - phi(n)^2);
+        if v >= 0.1
+            continue;
+        end
+        variance(m, n) = v;
+        load(sprintf(['../matfys/data/sv/normal_ret/lognormal_vol/q%.1f/Eig-' ...
+                      'sig%.4f-phi%.4f.mat'], q, sig(m), phi(n)), 'ev');
+
+        % ev = reshape(ev, 1, prod(size(ev)));
+        eigmax = max(ev)';
+        eigmin = min(ev)';
+        lambda_max = (1+sqrt(q))^2 * (1 + 2*v*(2*sqrt(q)+1));
+        lambda_min = (1-sqrt(q))^2 * (1 - 2*v*(2*sqrt(q)-1));
+        subplot(3, 3, c);
+        qqplot(eigmax);
+        grid on
+        title(sprintf(['average=%.2e v=%.4f\n'...
+                      ' mean=%.2e, std=%.2e'], ...
+                      mean(eigmax), v, lambda_max, std(eigmax)));
+        c = c + 1;
+        % lam2(m, n, :) = [mean(eigmax), (1+sqrt(q))^2 * ...
+        %                  (1 + 2*v*(2*sqrt(q)+1))];
+        % lam1(m, n, :) = [mean(eigmin), (1-sqrt(q))^2 * ...
+        %                  (1 - 2*v*(2*sqrt(q)-1))];
+        
     end
 end
-xlabel('log_{10}a');
-legend('the integral', 'y=1/a^2');
-grid on
+
