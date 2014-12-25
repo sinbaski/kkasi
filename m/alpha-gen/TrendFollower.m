0bb9abbd1b649de607a3cd9a39bb986682d90bb0
Xie Xiaolei <xie.xiaolei@gmail.com>
2014-12-25 19:23:48 +0100

    A 'buy' signal is triggered when the price is above the 95% quantile of the past 3 months' prices and the return is below the 5% quantile of the past 3 months' returns.

--- m/alpha-gen/TrendFollower.m
+++ m/alpha-gen/TrendFollower.m
@@ -1,44 +1,73 @@
 clear all
-close all
+% close all
 mysql = get_mysql();
+data = cell2mat(fetch(mysql, ['select low, closing, high from DAX order by ' ...
+                    'day desc limit 1000;']));
+data = flipud(data);
+close(mysql);
 
-closing = cell2mat(fetch(mysql, ['select closing from DAX order by ' ...
-                    'day desc limit 3000;']));
-closing = log(flipud(closing));
+%% correlation with large returns
+N = size(data, 1);
+closing = data(1:end, 2);
+ret = price2ret(closing);
+% ranges = log(data(1:end, 3)) - log(data(1:end, 1));
+Jup = NaN(N-59, 1);
+Jdown = NaN(N-59, 1);
+Short = NaN(N-59, 1);
+c = 1;
+d = 1;
+e = 1;
+A = [];
+B = [];
+% C = [];
+for k = 60 : N-1
+    if isempty(A)
+        A = closing(k-59 : k);
+        B = price2ret(A);
+    else
+        A = [A(2:end); closing(k)];
+        B = [B(2:end); price2ret(closing(k-1:k))];
+    end
+    % C = price2ret(closing(k-9:9:k))/9;    
+    if (sum(A <= closing(k)) <= 3 && ...
+        ret(k) >= quantile(B, 0.9))
+        Jup(c) = k+1;
+        c = c + 1;
+    elseif (ret(k) <= quantile(B, 0.1) &&...
+            sum(A >= closing(k)) <= 3)
+        Jdown(d) = k+1;
+        d = d + 1;
+    % elseif (sum(A > closing(k)) <= 3 &&...
+    %         C > quantile(B, 0.75) &&...
+    %         ret(k) < 0)
+    %     Short(e) = k+1;
+    %     e = e + 1;
+    end
+end
+Jup = Jup(1 : c - 1);
+Jdown = Jdown(1 : d - 1);
+% Short = Short(1 : e-1);
 
-ret = closing(2:end) - closing(1:end-1);
+plot(1:N, data(1:end, 2), Jup, data(Jup, 2), 'go',...
+     Jdown, data(Jdown, 2), 'ro');
+grid on
 
-%% trend def. 1. irrelevant
-% T1 = 10;
-% T2 = 20;
-% ind = NaN(length(ret)-T1+1, 1);
-% for d = T1 : length(ret)
-%     ind(d-T1+1) = sum(ret(d-T1+1:d) > 0)/T1 - 0.5;
+% N = size(data, 1);
+% indicator = NaN(sum(I <= length(closing) - T3)-2, 1);
+% target = NaN(sum(I <= length(closing) - T3)-2, 1);
+
+% I = I(1:length(target)+2);
+% for k = 3 : length(I)
+%     indicator(k-2) = (ret(I(k)) -sum(ret(I(k-2:k-1))))/3;
+%     target(k-2) = closing(I(k)+1+T3) - closing(I(k)+1);
 % end
 
-% ret20 = closing(T1+T2:end) - closing(T1:end-T2);
-
-% ind = ind(1:length(ret20));
-
-
-%% trend def. 2. irrelevant
-% T1 = 10;
-% T2 = 60;
-% horizon = 20;
-% ind = NaN(length(closing) - horizon - T2 +1, 1);
-% for d = T2 : length(closing) - horizon
-%     ind(d-T2+1) = mean(closing(d-T1+1:d)) - mean(closing(d-T2+1:d));
-% end
-
-% ret20 = closing(T2+horizon:end) - closing(T2:end-horizon);
+% plot(1:N, data(:, 2), I95, data(I95, 2), 'go', I5, data(I5, 2), 'ro');
+% grid on
 
 
-%% Correlation with the ranges
-% data = cell2mat(fetch(mysql, ['select low, closing, high from DAX order by ' ...
-%                     'day desc limit 250;']));
-% data = flipud(data);
-% close(mysql);
-% % ret = price2ret(data(:, 2));
+%% Correlation with ranges
+% ret = price2ret(data(:, 2));
 % ranges = log(data(:, 3)) - log(data(:, 1));
 % closing = log(data(:, 2));
 % T = 15;
@@ -51,23 +80,23 @@ ret = closing(2:end) - closing(1:end-1);
 % end
 % plot(1:T, rhos, 'o', 1:T, levels, 'r-', 1:T, -levels, 'r-');
 
-
+%% correlation between returns of different time scales.
 % closing = log(flipud(closing));
-T = 60;
-horizons = [1, 5, 10, 15, 20, 25];
-rhos = NaN(T-1, 1);
-for m = 1 : length(horizons)
-    h = horizons(m);
-    retH = closing(T+h : end) - closing(T : end-h);
-    N = length(retH);
+% T = 60;
+% horizons = [1, 5, 10, 15, 20, 25];
+% ret1 = closing(2:end) - closing(1:end-1);
+% rhos = NaN(T-1, 1);
+% for m = 1 : length(horizons)
+%     h = horizons(m);
+%     retH = closing(T+h : end) - closing(T : end-h);
+%     N = length(retH);
 
-    for k = 1 : T-1
-        rhos(k) = corr(ret(T-k : T-k+N-1), retH);
-    end
-    subplot(3, 2, m);
-    plot(1:T-1, rhos, 'o', ...
-         1:T-1, 2*ones(1, T-1)./sqrt(N), ...
-         1:T-1, -2*ones(1, T-1)./sqrt(N));
-    title(sprintf('%d day horizon', h));
-end
-
+%     for k = 1 : T-1
+%         rhos(k) = corr(ret1(T-k : T-k+N-1), retH);
+%     end
+%     subplot(3, 2, m);
+%     plot(1:T-1, rhos, 'o', ...
+%          1:T-1, 2*ones(1, T-1)./sqrt(N), ...
+%          1:T-1, -2*ones(1, T-1)./sqrt(N));
+%     title(sprintf('%d day horizon', h));
+% end
