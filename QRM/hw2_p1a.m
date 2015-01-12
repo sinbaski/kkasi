148b7bf4014dbaf475951f3789e414c93db4dcff
lxb353 <lxb353@ku.dk>
2015-01-12 16:07:30 +0100

    Use the data set given on Jeffrey's homepage.

--- QRM/hw2_p1a.m
+++ QRM/hw2_p1a.m
@@ -1,21 +1,41 @@
 clear all
 close all
-mysql = get_mysql();
+% mysql = get_mysql();
 X = NaN(1700, 2);
-assets ={'DAX', 'BMW_DE'};
-for k = 1 : size(X, 2)
-    data = cell2mat(fetch(mysql, sprintf(['select closing from %s order ' ...
-                        'by day desc limit 1701'], assets{k})));
-    X(:, k) = price2ret(flipud(data));
-end
-close(mysql);
+% assets ={'DAX', 'BMW_DE'};
+% for k = 1 : size(X, 2)
+%     data = cell2mat(fetch(mysql, sprintf(['select closing from %s order ' ...
+%                         'by day desc limit 1701'], assets{k})));
+%     X(:, k) = price2ret(flipud(data));
+% end
+% close(mysql);
+A = importdata('DAX.txt');
+X(:, 1) = A(:, 2);
+
+A = importdata('bmw_returns.txt');
+X(:, 2) = A(:, 2);
+
 C = cov(X);
 B = chol(C)';
 Y = inv(B) * (X - repmat(mean(X), size(X, 1), 1))';
+Y = Y';
 
-T1 = trnd(3.5, 2, size(Y, 2));
-X1 = B * T1 + repmat(mean(X)', 1, size(T1, 2));
-L1 = -[100, 100] * X1;
+% Z = NaN(size(Y));
+% angles = NaN(size(Y, 1), 1);
+% for k = 1 : size(Z, 1)
+%     Z(k, :) = Y(k, :) ./ norm(Y(k, :));
+%     angles(k) = angle(Z(k, 1) + i*Z(k, 2));
+% end
+% Tsim = trnd(3.2, size(Y, 1), 2);
+
+Tsim = trnd(3.2, 40000, 2);
+Xsim = Tsim * B';
+W = 100 * ones(1, 2);
+L = -Xsim * W';
+
+
+% X1 = B * T1 + repmat(mean(X)', 1, size(T1, 2));
+% L1 = -[100, 100] * X1;
 
 
 % % angles = NaN(1, size(Z, 1));
