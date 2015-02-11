e0379b08673fb747813f2ffffa8ea494beb98f82
Xie Xiaolei <xie.xiaolei@gmail.com>
2015-02-11 02:44:08 +0100

    Corrected the file for computing and plotting the empirical spectral density function and Tracy-Widom.

--- r/test.r
+++ r/test.r
@@ -2,14 +2,11 @@
 
 library(R.matlab);
 library(RMTstat)
-## Matlab$startServer();
-## matlab <- Matlab();
-## open(matlab);
 phi <- c(0, 0.5, 0.8160);
+# phi <- 0;
 sig <- c(0.1, 0.2, 0.5);
-q <- 0.5;
-m <- 3;
-n <- 3;
+q <- 0.1;
+# sig <- 0.5;
 pdf(sprintf("eigmax_TW_q%.2f.pdf", q));
 par(mfrow=c(3, 3));
 for (m in 1:length(sig)) {
@@ -23,15 +20,20 @@ for (m in 1:length(sig)) {
         for (k in 1:length(eigmax)) {
             eigmax[k] <- max(data$ev[, k])
         }
-        ## Par = WishartMaxPar(ndf=data$T, pdim=dim(data$ev)[1],
-        ##     var=exp(v/2), beta=1);
-        ## eigmax = (eigmax - Par$centering) / Par$scaling;
-        plot(density(eigmax), col='blue');
-        X = seq(from=min(eigmax), to=max(eigmax), length.out=400);
-        den = dWishartMax(X, ndf=data$T, pdim=dim(data$ev)[1],
-            var=exp(v/2), beta=1);
-        # den = dtw(X);
-        lines(den, col="red");
+        Par = WishartMaxPar(ndf=data$T, pdim=dim(data$ev)[1],
+            var=exp(2*v), beta=1);
+        eigmax = (eigmax - Par$centering) / Par$scaling;
+
+        epdf = density(eigmax);
+        plot(epdf$x, epdf$y, main=sprintf("v=%.4f", v),
+             xlab="X",
+             ylab="Densities",
+             col="blue", type="l");
+        # X = seq(from=min(eigmax), to=max(eigmax), length.out=400);
+        ## Y = dWishartMax(epdf$x, ndf=data$T, pdim=dim(data$ev)[1],
+        ##     var=exp(v), beta=1);
+        Y = dtw(epdf$x);
+        lines(epdf$x, Y, col="red");
         
         ## eigmax1 <- rWishartMax(length(eigmax), ndf=data$T, pdim=dim(data$ev)[1],
         ##                        var=exp(v/2), beta=1);
@@ -39,8 +41,7 @@ for (m in 1:length(sig)) {
     }
 }
 dev.off();
-# close(matlab);
-# rm(list = ls())
+
 
 
 
