7c4d4d505777692428e034071dcb4a169762007f
lxb353 <lxb353@ku.dk>
2015-05-28 18:28:53 +0200

    Revised the R script.

---
+++ r/libxxie.r
@@ -0,0 +1,35 @@
+library(RMySQL);
+getAssetReturns <- function(day1, day2, tables) {
+    database = dbConnect(MySQL(), user='sinbaski', password='q1w2e3r4',
+        dbname='avanza', host=Sys.getenv("PB"));
+    days <- vector('character');
+    for (i in 1:length(tables)) {
+        results <- dbSendQuery(
+            database,
+            sprintf("select distinct day from %s where
+day between '%s' and '%s' order by day", tables[i], day1, day2));
+        D = fetch(results, n=-1)[[1]];
+        dbClearResult(results);
+        if (length(days) > 0) {
+            days <- intersect(days, D);
+        } else {
+            days <- D;
+        }
+    }
+    n.days = length(days);
+    R = matrix(nrow=n.days-1, ncol=length(tables));
+    str = sprintf("'%s'", days[1]);
+    for (i in 2 : n.days) {
+        str = sprintf("%s, '%s'", str, days[i]);
+    }
+
+    for (i in 1:length(tables)) {
+        results <- dbSendQuery(database, sprintf("select closing from
+%s where day in (%s) order by day;", tables[i], str));
+        prices <- log(fetch(results, n=-1)[[1]]);
+        # R[,i] <- A[-1] - A[-n.days];
+        R[,i] = tail(prices, -1) - head(prices, -1);
+    }
+    dbDisconnect(database);
+    return (data.frame(days[-1], R));
+}
--- r/model2.r
+++ r/model2.r
@@ -1,8 +1,11 @@
 library(RMySQL);
 rm(list=ls());
 
-n.obs = 1000;
-last.day = "2015-05-22";
+source("libxxie.r");
+
+day2 = '2015-05-28';
+day1 = '2011-01-01';
+
 database = dbConnect(MySQL(), user='sinbaski', password='q1w2e3r4',
     dbname='avanza', host=Sys.getenv("PB"));
 
@@ -11,31 +14,26 @@ results = dbSendQuery(database, sprintf("select tblname from %s;", assetSet));
 tables <- fetch(results, n=-1)[[1]];
 p = length(tables);
 dbClearResult(results);
-
-R <- matrix(nrow=n.obs, ncol=length(tables));
-for (j in 1 : length(tables)) {
-    results <- dbSendQuery(database, sprintf("select closing from %s
-where day <= '%s' order by day desc limit %d;", tables[j], last.day, n.obs+1));
-    prices <- rev(log(fetch(results, n=-1)[[1]]));
-    dbClearResult(results);
-    R[,j] <- tail(prices, -1) - head(prices, -1);
-}
 dbDisconnect(database);
+ret <- getAssetReturns(day1, day2, tables);
+R = matrix(unlist(ret[, -1]), nrow=dim(ret)[1], byrow=FALSE);
 
-C <- t(R) %*% R / n.obs;
+C <- t(R) %*% R / dim(R)[1];
 E <- eigen(C, symmetric=TRUE);
 M <- E$vectors;
 for (k in 1 : dim(M)[2]) {
     M[,k] <- M[,k]/sum(abs(M[,k]));
 }
 factors <- R %*% M;
-models <- list();
+models <- matrix(NA, nrow=p+1, ncol=p);
 par(mfrow=c(3,4));
 for (k in 1:dim(M)[2]) {
     mdl <- lm(R[,k] ~ factors);
-    models <- c(models, mdl);
+    models[,k] <- mdl$coef / sum(abs(mdl$coef));
     acf(mdl$residuals, main=tables[k]);
 }
+
+A = R[, 10] - factors %*% models[-1,10];
 
 
 
