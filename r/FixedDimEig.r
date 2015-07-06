0e6ec2f115041bb1411059e9914a2a5197576077
lxb353 <lxb353@ku.dk>
2015-07-06 16:40:22 +0200

    The first version of the paper.

--- r/FixedDimEig.r
+++ r/FixedDimEig.r
@@ -2,7 +2,7 @@ rm(list=ls());
 library(FMStable)
 
 alpha <- 0.8;
-ns <- seq(2200, 5400, by=800);
+ns <- seq(1000, 2000, by=200);
 supremum <- rep(NA, length(ns));
 
 c <- 1;
@@ -10,6 +10,8 @@ for (n in ns) {
     p <- 5*floor(log(n));
     load(sprintf("dat/P-5xlogN_Dist-t/lambda.1.alpha%.1f_n%d.dat", alpha, n));
     X <- sort(lambda.1);
+    pars <- setMomentsFMstable(mean=0, sd=1, alpha=alpha/2);
+    F <- pEstable(X, pars)^p;
     supremum[c] <- max(c(abs(F[-p] - seq(1, p-1) / p), abs(F[-1] - seq(1, p-1) / p)));
     c <- c + 1;
 }
