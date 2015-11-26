61263cdc350b73f5f05ec964de93a6d1f54c01de
lxb353 <lxb353@ku.dk>
2015-11-26 14:00:23 +0100

    Empirical studies of the correlation matrix.

--- r/CorrelationMatrix.r
+++ r/CorrelationMatrix.r
@@ -1,5 +1,6 @@
 rm(list=ls());
 source("libxxie.r");
+## source("liblxb353.r");
 
 day2 = '2015-02-28';
 day1 = '2010-01-01';
@@ -24,28 +25,39 @@ E.adjusted <- eigen(t(R.adjusted) %*% R.adjusted);
 E.adjusted$values <- abs(E.adjusted$values);
 
 
-pdf("Eigenvalues_Cor_n_Cov_Matrix.pdf")
-plot(1:p, log10(E.adjusted$values), col="#FF0000", type="b",
-     xlab="i", ylab=expression(log[10](lambda[(i)])));
-points(1:p, log10(E$values), type="b", col="#000000");
+pdf("Eigenvalues_Cor_vs_Cov_Matrix.pdf")
+plot(log10(E$values), log10(E.adjusted$values), ylim=c(0,5), xlim=c(-2.5, 2.5),
+     xlab=expression(log[10](lambda^{cov})), ylab=expression(log[10](lambda^{corr})));
 grid(nx=10);
-legend("bottomleft", legend=c("Correlation Matrix", "Covariance Matrix"),
-       pch=c(1, 1), col=c("#FF0000", "#000000"));
 dev.off();
 
-## pdf("V1_CovarianceMatrix.pdf");
-## plot(1:p, E$vectors[,1], xlab="k", ylim=c(-0.1, -0.01),
-##      ylab=expression(V[k1]),
-##      main="Eigenvector of the largest eigenvalue");
+
+## pdf("Eigenvalues_Cor_n_Cov_Matrix.pdf")
+## plot(1:p, (E.adjusted$values), col="#FF0000", type="b",
+##      ylim=c(0, 1.0E4),
+##      xlab="i", ylab=expression(lambda[(i)]));
+## points(1:p, (E$values), type="b", col="#000000");
 ## grid(nx=10);
+## legend("topright", legend=c("Corr. Matrix", "Cov. Matrix"),
+##        pch=c(1, 1), col=c("#FF0000", "#000000"));
 ## dev.off();
 
-## pdf("V1_CorrelationMatrix.pdf");
-## plot(1:p, E.adjusted$vectors[,1], xlab="k", ylim=c(-0.1, -0.01),
-##      ylab=expression(V[k1]),
-##      main="Eigenvector of the largest eigenvalue");
-## grid(nx=10);
-## dev.off();
+
+for (i in 1:6) {
+    pdf(sprintf("V%d_CovarianceMatrix.pdf", i));
+    plot(1:p, E$vectors[,i], xlab="k",
+         ylab="kth comp",
+         main=sprintf("Eigenvector of the %dth largest eigenvalue", i));
+    grid(nx=10);
+    dev.off();
+
+    pdf(sprintf("V%d_CorrelationMatrix.pdf", i));
+    plot(1:p, E.adjusted$vectors[,2], xlab="k",
+         ylab="kth comp",
+         main=sprintf("Eigenvector of the %dth largest eigenvalue", i));
+    grid(nx=10);
+    dev.off();
+}
 
 ## pdf("Eigenvalues_of_CovarianceMatrix_SP500.pdf");
 ## plot(1:(p-1), log10(E$values[2:p]/E$values[1:(p-1)]), type="b",
