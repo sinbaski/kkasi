9194821442a0d321039bccac50e39b8972aaf0cd
Xie Xiaolei <xie@math.ku.dk>
2017-06-05 16:38:27 +0200

    Added functionalities to the program for Johannes.

--- papers/Johannes/prog1.cpp
+++ papers/Johannes/prog1.cpp
@@ -5,43 +5,44 @@
 #include <iterator>
 #include <assert.h>
 #include <iostream>
+#include <random>
 #include <vector>
 
 using namespace std;
 using namespace arma;
 
+random_device randev;
+
 class Lindep
 {
 protected:
-    vector<vector<double>> h;
 public:
+    mat h; // coefficients' matrix
+    double alpha = 1.5; // the tail index
     Lindep(mat &h);
     virtual mat H(unsigned s, unsigned n_rows, unsigned n_cols);
     virtual mat M(unsigned s, unsigned n_rows, unsigned n_cols);
     virtual mat K(unsigned s1, unsigned s2, unsigned n_rows, unsigned n_cols);
+    virtual mat C(mat &X, unsigned s);
+    virtual mat& sim_X(mat &X);
 };
 
 Lindep::Lindep(mat &h)
+    :h(h)
 {
-    // this->h.resize(h.n_rows);
-    // for (unsigned i = 0; i < h.n_rows; i++) {
-    // 	this->h[i].resize(h.n_cols);
-    // 	copy(h.begin_row(i), h.end_row(i), this->h[i].begin());
-    // }
 }
 
 mat Lindep::H(unsigned s, unsigned n_rows, unsigned n_cols)
 {
-    mat T = zeros(n_rows, n_cols);
-    if (s >= h[0].size()) return T;
-    // for (unsigned i = 0; i < h.size(); i++) {
-    // 	copy(
-    // 	    next(h[i].begin(), s),
-    // 	    h[i].size() - s < T.n_cols ? h[i].end() : next(h[i].begin(), s + T.n_cols),
-    // 	    T.begin_row(i)
-    // 	    );
-    // }
-    return T;
+    mat T = zeros(n_cols, n_rows);
+    if (s >= h.n_cols) return T.t();
+    mat g = h.t();
+    for (unsigned i = 0; i < h.n_rows; i++) {
+	double *p = g.colptr(i);
+	double *q = T.colptr(i);
+    	copy(p + s, h.n_cols - s < T.n_cols ? p + h.n_cols : p + s + T.n_cols, q);
+    }
+    return T.t();
 }
 
 mat Lindep::M(unsigned s, unsigned n_rows, unsigned n_cols)
@@ -61,18 +62,38 @@ mat Lindep::K(unsigned s1, unsigned s2, unsigned n_rows, unsigned n_cols)
     return T;
 }
 
-// mat K(unsigned s1, unsigned s2, mat &D, vec &c_bar)
-// {
-//     vec squared(s2 - s1 + 1);
-//     transform(
-// 	c_bar.begin() + s1, c_bar.begin() + s2 + 1,
-// 	squared.begin(),
-// 	[&](double x) {
-// 	    return x*x;
-// 	});
-//     double a = accumulate(squared.begin(), squared.end(), double(0));
-//     return a * D;
-// }
+mat Lindep::C(mat &X, unsigned s)
+{
+    
+}
+
+mat& Lindep::sim_X(mat &X)
+{
+    unsigned p = X.n_rows;
+    unsigned n = X.n_cols;
+    student_t_distribution<double> stud(alpha);
+    mat Z(p + h.n_rows, n + h.n_cols);
+
+    for (unsigned i = 0; i < Z.n_rows; i++) {
+#pragma omp parallel for
+	for (unsigned j = 0; j < Z.n_cols; j++) {
+	    Z(i, j) = stud(randev);
+	}
+    }
+    mat g = h.t();
+    for (unsigned i = 0; i < h.n_rows; i++) {
+	double *ptr = g.colptr(i);
+	reverse(ptr, ptr + h.n_cols);
+    }
+    g = g.t();
+    for (unsigned i = 0; i < p; i++) {
+#pragma omp parallel for
+	for (unsigned j = 0; j < n; j++) {
+	    X(i, j) = sum(sum(Z.submat(i, j, i + h.n_rows - 1, j + h.n_cols - 1) % g));
+	}
+    }
+    return X;
+}
 
 int main(int argc, char *argv[])
 {
@@ -95,20 +116,31 @@ int main(int argc, char *argv[])
     // mat D = d * d.t();
 
     Lindep lindep(h);
-    // vec val;
-    // mat modal;
+    vec val;
+    mat modal;
 
-    // mat K00 = lindep.K(0, 0, 3, 3);
-    // cout << "matrix K00" << endl;
-    // K00.print();
+    mat K00 = lindep.K(0, 0, 3, 3);
+    cout << "matrix K00" << endl;
+    K00.print();
 
-    // eig_sym(val, modal, K00);
-    // cout << "eigenvalues of  K00" << endl;
-    // val.print();
+    eig_sym(val, modal, K00);
+    cout << "eigenvalues of  K00" << endl;
+    val.print();
 
-    // cout << "eigenvectors of  K00" << endl;
-    // modal.print();
+    for (unsigned i = 0; i < modal.n_cols; i++) {
+	double sign = modal(0, i) >= 0 ? 1 : -1;
+	transform(modal.begin_col(i), modal.end_col(i), modal.begin_col(i),
+		  [=](double x) {
+		      return x * sign;
+		  });
+    }
+    cout << "eigenvectors of  K00" << endl;
+    modal.print();
 
+    mat X(200, 2000);
+    lindep.sim_X(X);
+    cout << "Matrix X:" << endl;
+    X.submat(0, 0, 10, 10).print();
 
     return 0;
 }
