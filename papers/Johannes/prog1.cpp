3447461abaceac47ff57598211fb6e2e14e87378
Xie Xiaolei <xie@math.ku.dk>
2017-06-05 21:35:59 +0200

    The result of 4.(1) is produced.

--- papers/Johannes/prog1.cpp
+++ papers/Johannes/prog1.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_cdf.h>
 
 using namespace std;
 using namespace arma;
@@ -19,17 +21,22 @@ protected:
 public:
     mat h; // coefficients' matrix
     double alpha = 1.5; // the tail index
+    double beta = 0.5;
+    mat *X;
+    
     Lindep(mat &h);
+    mat &set_X(mat *X);
     virtual mat H(unsigned s, unsigned n_rows, unsigned n_cols);
     virtual mat M(unsigned s, unsigned n_rows, unsigned n_cols);
     virtual mat K(unsigned s1, unsigned s2, unsigned n_rows, unsigned n_cols);
-    virtual mat C(mat &X, unsigned s);
+    virtual mat C(unsigned s);
     virtual mat& sim_X(mat &X);
 };
 
 Lindep::Lindep(mat &h)
-    :h(h)
+    :h(h), X(NULL)
 {
+    
 }
 
 mat Lindep::H(unsigned s, unsigned n_rows, unsigned n_cols)
@@ -62,9 +69,9 @@ mat Lindep::K(unsigned s1, unsigned s2, unsigned n_rows, unsigned n_cols)
     return T;
 }
 
-mat Lindep::C(mat &X, unsigned s)
+mat Lindep::C(unsigned s)
 {
-    
+    return X->submat(0, 0, X->n_rows - 1, X->n_cols - s - 1) * X->submat(0, s, X->n_rows - 1, X->n_cols - 1).t();
 }
 
 mat& Lindep::sim_X(mat &X)
@@ -80,12 +87,21 @@ mat& Lindep::sim_X(mat &X)
 	    Z(i, j) = stud(randev);
 	}
     }
+    // cout << "Matrix Z:" << endl;
+    // Z.print();
+
+    // cout << "Matrix h:" << endl;
+    // h.print();
+
     mat g = h.t();
     for (unsigned i = 0; i < h.n_rows; i++) {
 	double *ptr = g.colptr(i);
 	reverse(ptr, ptr + h.n_cols);
     }
     g = g.t();
+
+    // cout << "Matrix g:" << endl;
+    // g.print();
     for (unsigned i = 0; i < p; i++) {
 #pragma omp parallel for
 	for (unsigned j = 0; j < n; j++) {
@@ -95,25 +111,52 @@ mat& Lindep::sim_X(mat &X)
     return X;
 }
 
-int main(int argc, char *argv[])
+mat &Lindep::set_X(mat *X)
+{
+    this->X = X;
+    return *X;
+}
+
+void case_iid(void)
+{
+    mat h({1});
+    
+#pragma omp parallel for
+    for (unsigned n = 1000; n < 40000; n+=1000) {
+	Lindep lindep(h);
+	unsigned p = ceil(pow(n, lindep.beta));
+	mat X(p, n);
+	lindep.sim_X(X);
+	lindep.set_X(&X);
+
+	// cout << "matrix X" <<endl;
+	// X.submat(0, 0, 9, 12).print();
+
+	mat C0 = lindep.C(0);
+	mat T = C0;
+
+	unsigned s2 = 5;
+	for (unsigned s = 1; s <= s2; s++) {
+	    T += lindep.C(s);
+	}
+	double a_np = gsl_cdf_tdist_Pinv(1 - 1/(double)X.n_cols, lindep.alpha);
+	double y = norm(T - C0)/a_np/a_np;
+	// cout << "matrix T" << endl;
+	// T.print();
+
+	// cout << "matrix C0" << endl;
+	// C0.print();
+
+#pragma omp critical
+	printf("%5u\t%e\n", n, y);
+    }
+}
+
+void case_email(void)
 {
     vec c = {1, 0.5, 0.25};
     vec d = {1, 0.3, 0.09};
-    // unsigned LEN = c.n_elem;
-    
-    // vec c_bar(LEN);
-    // for (unsigned s = 0; s < LEN; s++) {
-    // 	vec temp = zeros(LEN);
-    // 	vec temp2(LEN);
-    // 	copy(c.begin() + s, c.end(), temp.begin());
-    // 	transform(
-    // 	    c.begin(), c.end(), temp.begin(), temp2.begin(),
-    // 	    std::multiplies<double>()
-    // 	    );
-    // 	c_bar[s] = accumulate(temp2.begin(), temp2.end(), double(0));
-    // }
     mat h = kron(d, c.t());
-    // mat D = d * d.t();
 
     Lindep lindep(h);
     vec val;
@@ -141,6 +184,10 @@ int main(int argc, char *argv[])
     lindep.sim_X(X);
     cout << "Matrix X:" << endl;
     X.submat(0, 0, 10, 10).print();
+}
 
+int main(int argc, char *argv[])
+{
+    case_iid();
     return 0;
 }
