629219884469373e32c0aea5d90eb6e68870b16e
Xie Xiaolei <xie@math.ku.dk>
2017-09-15 20:24:30 +0200

    Fixed the problem raised by Olivier.

--- cpp/ImportanceSampling/test.r
+++ cpp/ImportanceSampling/test.r
@@ -1,8 +1,8 @@
 rm(list=ls())
 R <- read.table("path_stat.dat");
-## pdf("../../papers/Jeffrey1/SP500_r.pdf");
+pdf("/tmp/right_eigenfunction.pdf");
 plot(R[, 1], R[, 2], type="l",
      main=expression(r[xi]),
      xlab="x", ylab="y");
-## dev.off();
+dev.off();
 
